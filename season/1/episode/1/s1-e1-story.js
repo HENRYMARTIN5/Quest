3c8e860999ced4640ee3d2e392135a8a52da4388
HENRYMARTIN5 <s9028524@milwaukee.k12.wi.us>
2020-04-15 16:39:45 +0000

    started story

--- season/1/episode/1/s1-e1-story.js
+++ season/1/episode/1/s1-e1-story.js
@@ -1,9 +1,26 @@
-
-function doStory() {
-	alertify.alert().setting('modal', false);
-  alertify.confirm().setting('modal', false);
-	alertify.alert("???", "Wake up... Please wake up...");
-	alertify.alert("", "You sit up and groan.")
-	alertify.alert("You", "W-where am I?");
+alertify.set('notifier','position', 'bottom-left');
+function sleep(ms) {
+  return new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
 }
-document.onload(doStory());
+function say(text){
+	alertify.notify(text);
+}
+async function doStory() {
+		say("<b>???:</b>  Wake up... Please wake up...");
+		await sleep(1000);
+		say("<i>You sit up and groan.</i>");
+		await sleep(500)
+		say("<b>You:</b>  W-where am I?")
+		await sleep(1000)
+		say("<b>???:</b>  All in good time...")
+		await sleep(1000)
+		say("<b>???:</b>  Now, let's get you back on your feet.")
+		await sleep(1000)
+		say("<b>GAME:</b><i>  Use the W, A, S and D keys to move around.</i>")
+		await sleep(100)
+		say("<b>GAME:</b><i>  Click and drag with your mouse to look around.</i>")
+		
+}
+document.onload = doStory();
